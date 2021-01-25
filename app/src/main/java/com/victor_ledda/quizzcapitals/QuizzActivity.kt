f61698f162676057a92e1c431de7d5c1a8f09fb3
Victor <victor.ledda.vic@gmail.com>
2021-01-25 16:39:53 +0100

    Jour 3

--- app/src/main/java/com/victor_ledda/quizzcapitals/QuizzActivity.kt
+++ app/src/main/java/com/victor_ledda/quizzcapitals/QuizzActivity.kt
@@ -15,26 +15,37 @@ var currentQuizIndex: Int = 0
 
 
 class QuizzActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz)
-        quizs.add(Quiz("Quelle est la capitale de la France", "Alger","Paris","Milan","Mexico",1 ,2))
-        quizs.add(Quiz("Quelle est la capitale de l'Angleterre", "Paris","Londres","Sydney","Sao Paulo",2,2))
-        quizs.add(Quiz("Quelle est la capitale de l'Allemagne", "Moscou","Paris","Berlin","Los Angeles",5,3))
-        quizs.add(Quiz("Quelle est la capitale du Brésil", "Brasilia","Berlin","Turin","Constantinople",6, 1 ))
+        quizs.add(Quiz("Quelle est la capitale de la France ?", "Alger","Paris","Milan","Mexico" , R.drawable.paris,2))
+        quizs.add(Quiz("Quelle est la capitale de l'Angleterre ?", "Paris","Londres","Sydney","Sao Paulo", R.drawable.londres, 2))
+        quizs.add(Quiz("Quelle est la capitale de l'Allemagne ?", "Moscou","Paris","Los Angeles","Berlin",R.drawable.berlin, 4))
+        quizs.add(Quiz("Quelle est la capitale du Brésil ?", "Brasilia","Berlin","Turin","Constantinople", R.drawable.brasilia, 1 ))
+        quizs.add(Quiz("Quelle est la capitale de la Corée du nord ?", "Séoul","New Delhi","Moscou","Pyongyang" , R.drawable.coree,4))
+        quizs.add(Quiz("Quelle est la capitale du Nigéria ?", "Lomé","Khartoum","Lusuka","Niger", R.drawable.nigeria, 4))
+        quizs.add(Quiz("Quelle est la capitale de Mongolie ?", "Kinshasa","N'Djamena","Oulan-Batour","Dodoma",R.drawable.mongolie, 3))
+        quizs.add(Quiz("Quelle est la capitale du Pérou ?", "La paz","Asuncion","Lima","Santiago", R.drawable.perou, 3 ))
+        quizs.add(Quiz("Quelle est la capitale de nouvelle zélande ?", "Wellington","Sidney","Bangkok","Vientiane",R.drawable.zelande, 1))
+        quizs.add(Quiz("Quelle est la capitale du Cambodge ?", "Phnom Penh","Kuala Lumpur","Jakarta","Manille", R.drawable.cambodge, 1 ))
    showQuestion(quizs.get(currentQuizIndex))
     }
 
 
     fun showQuestion(quiz: Quiz) {
+
+
         val txtQuestion: TextView = findViewById(R.id.txtQuestion)
         val answer1: TextView = findViewById(R.id.answer1)
         val answer2: TextView = findViewById(R.id.answer2)
-        val answer3: TextView = findViewById(R.id.answer3)
+        val answer3: TextView = findViewById(R.id.ans3)
         val answer4: TextView = findViewById(R.id.answer4)
-        val imageHome: ImageView = findViewById(R.id.image);
+        val imageHome: ImageView = findViewById(R.id.image)
+
+
         txtQuestion.setText(quiz.question)
-        imageHome.setImageResource(quiz.imageHome)
+        imageHome.setImageResource(quiz.imageHome);
         answer1.setText(quiz.answer1)
         answer2.setText(quiz.answer2)
         answer3.setText(quiz.answer3)
@@ -45,15 +56,15 @@ fun handleAnswer(answerID: Int){
 
     if(quiz.isCorrect(answerID)){
         numberOfGoodAnswers++
-        Toast.makeText( this, "+1", Toast.LENGTH_SHORT).show()
+        Toast.makeText( this, "Bonne réponse", Toast.LENGTH_SHORT).show()
     }else{
-        Toast.makeText( this, "+0", Toast.LENGTH_SHORT).show()
+        Toast.makeText( this, "Mauvaise réponse", Toast.LENGTH_SHORT).show()
     }
     currentQuizIndex++
     if(currentQuizIndex>= quizs.size){
     var alert = AlertDialog.Builder(this )
         alert.setTitle("Partie terminée")
-        alert.setMessage("Tu as eu : " + numberOfGoodAnswers + "de bonnes réponses")
+        alert.setMessage("Tu as eu : " + numberOfGoodAnswers + "  bonnes réponses")
         alert.setPositiveButton("OK"){
             dialogInterface: DialogInterface?, i: Int -> finish()
         }
@@ -74,6 +85,9 @@ fun handleAnswer(answerID: Int){
     fun onClickAnswer4(view: View){
         handleAnswer(4)
     }
+
 }
+
+
 
 
